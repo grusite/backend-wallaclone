1aaf25673b21bad8b5ce70349018268e8c19c4cc
Jorge Martín <grusite@gmail.com>
2020-02-16 13:15:46 +0100

    added ads json and images

--- app.js
+++ app.js
@@ -1,52 +1,54 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const expressDeliver = require('express-deliver');
-const logger = require('morgan');
-const isDev = process.env.NODE_ENV === 'development';
-const exceptionPool = require('./lib/exceptionPool');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const db = require('./lib/db');
-const { printDeliverError, getMorganConfig } = require('./lib/utils');
+const express = require('express')
+const cookieParser = require('cookie-parser')
+const expressDeliver = require('express-deliver')
+const logger = require('morgan')
+const isDev = process.env.NODE_ENV === 'development'
+const exceptionPool = require('./lib/exceptionPool')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const db = require('./lib/db')
+const { printDeliverError, getMorganConfig } = require('./lib/utils')
 
-const userController = require('./controllers/userController');
-const userRouter = require('./routes/user');
-const adsRouter = require('./routes/apiv1/advertisements');
+const userController = require('./controllers/userController')
+const userRouter = require('./routes/user')
+const adsRouter = require('./routes/apiv1/advertisements')
 
-const app = express();
+const app = express()
 
 // I enable Access-Control-Allow-Origin: * in header
-app.use(cors());
+app.use(cors())
 // support parsing of application/json type post data
-app.use(bodyParser.json());
+app.use(bodyParser.json())
+// Static files
+app.use(express.static('public'))
 
 if (isDev) {
-  app.use(logger('dev', getMorganConfig()));
+  app.use(logger('dev', getMorganConfig()))
 }
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 
 expressDeliver(app, {
   printErrorStack: false,
   printInternalErrorData: isDev,
   exceptionPool,
-  onError: printDeliverError
-});
+  onError: printDeliverError,
+})
 
 app.use(function(req, res, next) {
-  if (db.connection.readyState !== 1) throw new exceptionPool.NoDatabase();
-  next();
-});
+  if (db.connection.readyState !== 1) throw new exceptionPool.NoDatabase()
+  next()
+})
 
 /**
  * Rutas de mi API
  */
-app.use('/', userController.loadUser);
-app.get('/', async () => 'It works');
-app.use('/user', userRouter);
-app.use('/apiv1/anuncios', adsRouter);
+app.use('/', userController.loadUser)
+app.get('/', async () => 'It works')
+app.use('/user', userRouter)
+app.use('/apiv1/anuncios', adsRouter)
 
-expressDeliver.errorHandler(app);
+expressDeliver.errorHandler(app)
 
-module.exports = app;
+module.exports = app
--- models/Advertisement.js
+++ models/Advertisement.js
@@ -1,36 +1,35 @@
-'use strict';
+'use strict'
 
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const advertisementSchema = mongoose.Schema({
-  name: String,
-  sold: Boolean,
-  price: Number,
-  picture: String,
-  tags: {
-    type: [String],
-    enum: ['lifestyle', 'motor', 'mobile', 'work']
+const advertisementSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true, max: 30, index: true },
+    description: { type: String, max: 100 },
+    type: { type: String, enum: ['buy', 'sell'], required: true, index: true },
+    price: { type: Number, required: true },
+    picture: { type: String, required: true },
+    tags: {
+      type: [String],
+      enum: ['lifestyle', 'motor', 'mobile', 'work'],
+      index: true,
+    },
+  },
+  {
+    // Añade las propiedades de created y updated
+    timestamps: true,
   }
-});
+)
 
-advertisementSchema.index({ tags: 1 });
-advertisementSchema.index({ sold: 1 });
-advertisementSchema.index({ price: 1 });
-advertisementSchema.index({ name: 1 });
+advertisementSchema.index({ type: 1, tags: 1 })
 
-advertisementSchema.statics.list = function({
-  filter,
-  skip,
-  limit,
-  fields,
-  sort
-}) {
+advertisementSchema.statics.list = function({ filter, skip, limit, fields, sort }) {
   const query = this.find(filter)
     .skip(skip)
     .limit(limit)
     .select(fields)
-    .sort(sort);
-  return query.exec();
-};
+    .sort(sort)
+  return query.exec()
+}
 
-module.exports = mongoose.model('Ad', advertisementSchema);
+module.exports = mongoose.model('Ad', advertisementSchema)
