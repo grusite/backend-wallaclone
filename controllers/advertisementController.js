2c28434c6a871b2d1081a0a636a109de85912002
Jorge Martín <grusite@gmail.com>
2020-02-20 00:39:10 +0100

    fixed advertisement filter problem and email sender

--- controllers/advertisementController.js
+++ controllers/advertisementController.js
@@ -1,4 +1,4 @@
-const Ad = require('../models/Advertisement');
+const Ad = require('../models/Advertisement')
 const { NotFound } = require('../lib/exceptionPool')
 
 module.exports = {
@@ -9,45 +9,45 @@ module.exports = {
    *  http://localhost:3000/apiv1/anuncios?limit=2&skip=2&fields=name
    */
   async listAds(req, res, next) {
-    const name = req.query.name;
-    const sold = req.query.sold;
-    const price = req.query.price;
-    const tag = req.query.tag;
-    const skip = parseInt(req.query.start);
-    const limit = parseInt(req.query.limit);
-    const fields = req.query.fields;
-    const sort = req.query.sort;
+    const name = req.query.name
+    const type = req.query.type
+    const price = req.query.price
+    const tag = req.query.tag
+    const skip = parseInt(req.query.start)
+    const limit = parseInt(req.query.limit)
+    const fields = req.query.fields
+    const sort = req.query.sort
 
-    const filter = {};
+    const filter = {}
 
     if (name) {
-      filter.name = new RegExp('^' + name, 'i');
+      filter.name = new RegExp('^' + name, 'i')
     }
 
-    if (typeof sold !== 'undefined') {
-      filter.sold = sold;
+    if (typeof type !== 'undefined') {
+      filter.type = type
     }
 
     if (price) {
-      const greater = /^[0-9]+-$/g.test(price);
-      const between = /^[0-9]+-[0-9]+$/g.test(price);
-      const less = /^-[0-9]+$/g.test(price);
+      const greater = /^[0-9]+-$/g.test(price)
+      const between = /^[0-9]+-[0-9]+$/g.test(price)
+      const less = /^-[0-9]+$/g.test(price)
 
-      const [gte, lte] = price.split('-');
+      const [gte, lte] = price.split('-')
 
       if (greater) {
-        filter.price = { $gte: gte };
+        filter.price = { $gte: gte }
       } else if (between) {
-        filter.price = { $gte: gte, $lte: lte };
+        filter.price = { $gte: gte, $lte: lte }
       } else if (less) {
-        filter.price = { $lte: lte };
+        filter.price = { $lte: lte }
       } else {
-        filter.price = price;
+        filter.price = price
       }
     }
 
     if (tag) {
-      filter.tags = { $in: tag };
+      filter.tags = { $in: tag }
     }
 
     const ads = await Ad.list({
@@ -55,8 +55,8 @@ module.exports = {
       skip,
       limit,
       fields,
-      sort
-    });
+      sort,
+    })
 
     return ads
   },
@@ -66,7 +66,7 @@ module.exports = {
    * Obtiene un agente
    */
   async listAdbyId(req, res, next) {
-    const ad = await Ad.findOne({_id: req.params.id});
+    const ad = await Ad.findOne({ _id: req.params.id })
     if (!ad) throw new NotFound()
     return ad
   },
@@ -76,7 +76,7 @@ module.exports = {
    * Devuelve los tags únicos existentes
    */
   async listTags(req, res, next) {
-    const uniqueTags = await Ad.find().distinct('tags');
+    const uniqueTags = await Ad.find().distinct('tags')
     return uniqueTags
   },
 
@@ -85,9 +85,9 @@ module.exports = {
    * Crear un anuncio
    */
   async addAd(req, res, next) {
-    const data = req.body;
-    const ad = new Ad(data);
-    const adSaved = await ad.save();
+    const data = req.body
+    const ad = new Ad(data)
+    const adSaved = await ad.save()
     return adSaved
   },
 
@@ -96,16 +96,12 @@ module.exports = {
    * Actualiza un anuncio
    */
   async updateAd(req) {
-    const _id = req.params.id;
-    const data = req.body;
+    const _id = req.params.id
+    const data = req.body
 
-    const adSaved = await Ad.findOneAndUpdate(
-      { _id },
-      data,
-      {
-        new: true
-      }
-    );
+    const adSaved = await Ad.findOneAndUpdate({ _id }, data, {
+      new: true,
+    })
 
     if (!adSaved) throw new NotFound()
     return adSaved
@@ -116,9 +112,9 @@ module.exports = {
    * Elimina un anuncio
    */
   async deleteAd(req) {
-    const ad = await Ad.findOne({ _id: req.params.id});
+    const ad = await Ad.findOne({ _id: req.params.id })
     if (!ad) throw new NotFound()
-    await ad.remove();
-    return { done: true, message: 'Deleted correctly' };
-  }
-};
+    await ad.remove()
+    return { done: true, message: 'Deleted correctly' }
+  },
+}
